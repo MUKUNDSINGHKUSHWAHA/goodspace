3e98019cf12ed5e10948d13b30acccfda91ea312
MUKUNDSINGHKUSHWAHA <mukundsinghkushwaha@gmail.com>
2024-09-15 00:43:22 +0530

    hero section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <HeroSection/>
     </>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import "../styles/heroSection.scss";
+
+const HeroSection = () => {
+  return (
+    <div className="hero_container">
+      <div className="hero_info">
+        <h3>The World’s Most Configurable</h3>
+        <h1>Travel & Expense Management Software</h1>
+        <p>
+          Configure <a>any</a> rule. Enable a world class mobile experience for
+          your users. Get seamless reporting & integrations. Inbuilt OCR expense
+          scanning. Manage travel & non-travel expenses. Open APIs to connect
+          with external systems.
+        </p>
+      </div>
+      <div className="hero_form">
+        <form>
+          <div>
+            <input placeholder="Name" />
+          </div>
+          <div>
+            <input placeholder="Official email ID" />
+          </div>
+          <div>
+            <button type="submit">Start trial</button>
+          </div>
+        </form>
+        <div className="trial_container">
+          <p>30 Days free trial &#x2022;</p>
+          <p> &nbsp; Upto 10 users</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
